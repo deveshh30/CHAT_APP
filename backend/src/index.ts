c48dda51afd350b1ad7aa00db0e421b43cabe284
deveshh30 <devesh30gupta@gmail.com>
2026-03-10 01:38:07 +0530

    feat: implement messaging functionality with message model, routes, and controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.ts"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.ts";
 import cookieParser from "cookie-parser"
+import messageRoute from "./routes/message.route.ts"
 
 const app = express();
 dotenv.config()
@@ -11,6 +12,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth" , authRoutes);
+app.use("/api/message" , messageRoute);
 
 app.listen(PORT , () => {
     console.log(`server is running on port : ${PORT} `);
